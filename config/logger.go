4a1f6c78ffb4339eb33953344c8ccd39e8c462a9
isaquecsilva <152451006+isaquecsilva@users.noreply.github.com>
2024-10-08 10:44:36 -0300

    Logger nows accepts an optional list of writers for its output, in case of empty call, the default os.Stderr is used. Later calls  - after first one - does not affect Loggers writer output.

--- config/logger.go
+++ config/logger.go
@@ -1,15 +1,28 @@
 package config
 
 import (
+	"io"
 	"log/slog"
-)
+	"os"
+	"sync"
 
+	"github.com/mylxsw/go-utils/ternary"
+)
 
 var (
 	// singleton
-	logger *slog.Logger = slog.Default()
+	logger *slog.Logger
+	once   sync.Once
 )
 
-func Logger() *slog.Logger {
+func Logger(w ...io.Writer) *slog.Logger {
+	once.Do(func() {
+		out := ternary.IfElse[io.Writer](len(w) != 0, io.MultiWriter(w...), os.Stderr)
+
+		logger = slog.New(slog.NewTextHandler(out, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	})
+
 	return logger
 }
